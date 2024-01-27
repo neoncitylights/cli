44e5f5d3aa36573b1a3dd3d7dc28ab3d6af99de9
Samantha Nguyen <contact@samanthanguyen.me>
2024-01-26 19:37:28 -0600

    chore: import more templates
    This imports the following repository templates:
    - neoncitylights/mdbook
    - neoncitylights/mediawiki-extension

---
+++ crates/neoncitylights/templates/mediawiki-extension/includes/Specials/SpecialHelloWorld.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace MediaWiki\Extension\ExtensionUc\Specials;
+
+use SpecialPage;
+
+/**
+ * @license MIT
+ */
+class SpecialHelloWorld extends SpecialPage {
+	/**
+	 * @inheritDoc
+	 */
+	public function __construct() {
+		parent::__construct( 'HelloWorld' );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function execute( $sub ) {
+		$this->setHeaders();
+		$this->outputHeader();
+		$this->getOutput()->addWikiMsg( '{{extension-lc}}-helloworld' );
+	}
+}
