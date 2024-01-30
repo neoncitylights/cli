3228d636e6b2581dfe96596bdf6635826be77386
Samantha Nguyen <contact@samanthanguyen.me>
2024-01-29 18:29:24 -0600

    feat: start initial implementation of backend
    First rough pass on the CLI backend, which starts with reusable interactive prompts.

---
+++ crates/crosstui_prompt/src/main.rs
@@ -0,0 +1,70 @@
+use crossterm::{
+	event::{read, Event, KeyCode, KeyEvent},
+	style::{style, StyledContent, Stylize},
+	terminal::{disable_raw_mode, enable_raw_mode},
+};
+use std::io::{stdout, Result, Write};
+
+fn main() -> Result<()> {
+	let _result1 = prompt_yesno("Would you like to use TypeScript?")?;
+	let _result2 = prompt_yesno("Would you like to use Rust?")?;
+	Ok(())
+}
+
+fn prompt_yesno(question: &str) -> Result<bool> {
+	let choices = &["yes", "no"];
+	prompt(question, choices, |s| s.green().bold(), |s| s.grey()).map(|s| s == choices[0])
+}
+
+#[rustfmt::skip]
+fn prompt<
+	OnSelected,
+	OnUnselected,
+>(
+	question: &str,
+	options: &[&str],
+	on_selected: OnSelected,
+	on_unselected: OnUnselected,
+
+) -> Result<String>
+where 
+	OnSelected: Fn(StyledContent<&str>) -> StyledContent<&str>,
+	OnUnselected: Fn(StyledContent<&str>) -> StyledContent<&str>
+{
+	enable_raw_mode()?;
+
+	let mut selected = 0;
+	loop {
+		print!("\r{} ", question);
+		for (i, option) in options.iter().enumerate() {
+			if i == selected {
+				print!("{}", on_selected(style(option)));
+			} else {
+				print!("{}", on_unselected(style(option)));
+			}
+			if i != options.len() - 1 {
+				print!(" / ");
+			}
+		}
+		stdout().flush()?;
+
+		match read()? {
+			Event::Key(KeyEvent { code: KeyCode::Right, ..}) |
+			Event::Key(KeyEvent { code: KeyCode::Down, ..})
+			=> {
+				selected = (selected + 1) % options.len();
+			}
+			Event::Key(KeyEvent { code: KeyCode::Left, ..}) |
+			Event::Key(KeyEvent { code: KeyCode::Up, .. })
+			=> {
+				selected = if selected > 0 { selected - 1 } else { options.len() - 1 };
+			}
+			Event::Key(KeyEvent { code: KeyCode::Enter, ..}) => break,
+			_ => {}
+		}
+	}
+
+	disable_raw_mode()?;
+	println!();
+	Ok(options[selected].to_string())
+}
